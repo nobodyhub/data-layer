d2301c4803f290e743cca6ad1cd1295e445efd73
javis-yan <yan_h@hotmail.com>
2018-04-21 22:51:28 +0800

    add test for DeepContainer

---
+++ src/test/java/com/nobodyhub/datalayer/core/AvroSchemaLoaderDeepContainerTest.java
@@ -0,0 +1,105 @@
+package com.nobodyhub.datalayer.core;
+
+import com.nobodyhub.datalayer.core.cases.*;
+import org.apache.avro.LogicalTypes;
+import org.apache.avro.Schema;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Ryan
+ */
+public class AvroSchemaLoaderDeepContainerTest {
+    @Test
+    public void testLoad() {
+        AvroSchemaLoader.load(
+                DeepContainer.class,
+                ComplexClass.class,
+                InheritedClass.class,
+                SimpleEnum.class,
+                PrimitiveClass.class,
+                PrimitiveContainerClass.class,
+                ComplexEnum.class);
+        assertEquals("{\"type\":\"record\",\"name\":\"DeepContainer\",\"namespace\":\"com.nobodyhub.datalayer.core.cases\",\"fields\":[{\"name\":\"aSimpleEnumMapList\",\"type\":[{\"type\":\"array\",\"items\":{\"type\":\"map\",\"values\":{\"type\":\"enum\",\"name\":\"SimpleEnum\",\"symbols\":[\"S1\",\"S2\",\"S3\"]}}},\"null\"]},{\"name\":\"aBigDecimalSetListMap\",\"type\":[{\"type\":\"map\",\"values\":{\"type\":\"array\",\"items\":{\"type\":\"map\",\"values\":{\"type\":\"bytes\",\"logicalType\":\"decimal\",\"precision\":19,\"scale\":2}}}},\"null\"]},{\"name\":\"aComplexClassListSetMapMapMap\",\"type\":{\"type\":\"map\",\"values\":{\"type\":\"map\",\"values\":{\"type\":\"map\",\"values\":{\"type\":\"array\",\"items\":{\"type\":\"map\",\"values\":{\"type\":\"record\",\"name\":\"ComplexClass\",\"fields\":[{\"name\":\"primitiveClass\",\"type\":{\"type\":\"record\",\"name\":\"PrimitiveClass\",\"fields\":[{\"name\":\"aString\",\"type\":[\"string\",\"null\"]},{\"name\":\"aByteBuffer\",\"type\":[\"bytes\",\"null\"]},{\"name\":\"aInt\",\"type\":\"int\"},{\"name\":\"aInteger\",\"type\":[\"int\",\"null\"]},{\"name\":\"along\",\"type\":[\"long\",\"null\"]},{\"name\":\"aLong\",\"type\":[\"long\",\"null\"]},{\"name\":\"afloat\",\"type\":[\"float\",\"null\"]},{\"name\":\"aFloat\",\"type\":[\"float\",\"null\"]},{\"name\":\"adouble\",\"type\":\"double\"},{\"name\":\"aDouble\",\"type\":[\"double\",\"null\"]},{\"name\":\"aboolean\",\"type\":[\"boolean\",\"null\"]},{\"name\":\"aBoolean\",\"type\":\"boolean\"},{\"name\":\"aBigDecimal\",\"type\":{\"type\":\"bytes\",\"logicalType\":\"decimal\",\"precision\":19,\"scale\":2}},{\"name\":\"aDate\",\"type\":{\"type\":\"int\",\"logicalType\":\"date\"}},{\"name\":\"aTimeStamp\",\"type\":{\"type\":\"long\",\"logicalType\":\"timestamp-millis\"}},{\"name\":\"aUuid\",\"type\":{\"type\":\"string\",\"logicalType\":\"uuid\"}}]}},{\"name\":\"primitiveContainerClass\",\"type\":{\"type\":\"record\",\"name\":\"PrimitiveContainerClass\",\"fields\":[{\"name\":\"aStringList\",\"type\":[{\"type\":\"array\",\"items\":\"string\"},\"null\"]},{\"name\":\"aByteBufferList\",\"type\":{\"type\":\"array\",\"items\":\"bytes\"}},{\"name\":\"aBooleanList\",\"type\":[{\"type\":\"array\",\"items\":\"boolean\"},\"null\"]},{\"name\":\"aFloatList\",\"type\":[{\"type\":\"array\",\"items\":\"float\"},\"null\"]},{\"name\":\"aIntegerMap\",\"type\":[{\"type\":\"map\",\"values\":\"int\"},\"null\"]},{\"name\":\"aLongMap\",\"type\":[{\"type\":\"map\",\"values\":\"long\"},\"null\"]},{\"name\":\"aDoubleMap\",\"type\":[{\"type\":\"map\",\"values\":\"double\"},\"null\"]},{\"name\":\"aBigDecimalMap\",\"type\":[{\"type\":\"map\",\"values\":{\"type\":\"bytes\",\"logicalType\":\"decimal\",\"precision\":19,\"scale\":2}},\"null\"]},{\"name\":\"aUuidSet\",\"type\":[{\"type\":\"array\",\"items\":{\"type\":\"string\",\"logicalType\":\"uuid\"}},\"null\"]},{\"name\":\"aDateSet\",\"type\":[{\"type\":\"array\",\"items\":{\"type\":\"int\",\"logicalType\":\"date\"}},\"null\"]},{\"name\":\"aTimestampSet\",\"type\":[{\"type\":\"array\",\"items\":{\"type\":\"long\",\"logicalType\":\"timestamp-millis\"}},\"null\"]}]}},{\"name\":\"simpleEnum\",\"type\":\"SimpleEnum\"},{\"name\":\"complexEnum\",\"type\":{\"type\":\"enum\",\"name\":\"ComplexEnum\",\"symbols\":[\"C1\",\"C2\",\"C3\"]}}]}}}}}}},{\"name\":\"aInheritedClassMapListMapMapSet\",\"type\":[{\"type\":\"array\",\"items\":{\"type\":\"map\",\"values\":{\"type\":\"map\",\"values\":{\"type\":\"array\",\"items\":{\"type\":\"map\",\"values\":{\"type\":\"record\",\"name\":\"InheritedClass\",\"fields\":[{\"name\":\"anotherString\",\"type\":[\"string\",\"null\"]},{\"name\":\"simpleEnum\",\"type\":\"SimpleEnum\"},{\"name\":\"primitiveClassList\",\"type\":{\"type\":\"array\",\"items\":\"PrimitiveClass\"}},{\"name\":\"aString\",\"type\":[\"string\",\"null\"]},{\"name\":\"aByteBuffer\",\"type\":[\"bytes\",\"null\"]},{\"name\":\"aInt\",\"type\":\"int\"},{\"name\":\"aInteger\",\"type\":[\"int\",\"null\"]},{\"name\":\"along\",\"type\":[\"long\",\"null\"]},{\"name\":\"aLong\",\"type\":[\"long\",\"null\"]},{\"name\":\"afloat\",\"type\":[\"float\",\"null\"]},{\"name\":\"aFloat\",\"type\":[\"float\",\"null\"]},{\"name\":\"adouble\",\"type\":\"double\"},{\"name\":\"aDouble\",\"type\":[\"double\",\"null\"]},{\"name\":\"aboolean\",\"type\":[\"boolean\",\"null\"]},{\"name\":\"aBoolean\",\"type\":\"boolean\"},{\"name\":\"aBigDecimal\",\"type\":{\"type\":\"bytes\",\"logicalType\":\"decimal\",\"precision\":19,\"scale\":2}},{\"name\":\"aDate\",\"type\":{\"type\":\"int\",\"logicalType\":\"date\"}},{\"name\":\"aTimeStamp\",\"type\":{\"type\":\"long\",\"logicalType\":\"timestamp-millis\"}},{\"name\":\"aUuid\",\"type\":{\"type\":\"string\",\"logicalType\":\"uuid\"}}]}}}}}},\"null\"]}]}",
+                AvroSchemaLoader.schemas.get("com.nobodyhub.datalayer.core.cases.DeepContainer").toString());
+    }
+
+    @Test
+    public void testParseClass() {
+        AvroRecord record = AvroSchemaLoader.parseClass(DeepContainer.class);
+        assertEquals(DeepContainer.class, record.getClazz());
+        assertEquals("DeepContainer", record.getSimpleName());
+        assertEquals("com.nobodyhub.datalayer.core.cases.DeepContainer", record.getQualifiedName());
+        assertEquals("com.nobodyhub.datalayer.core.cases", record.getNamespace());
+        assertEquals(4, record.getFields().size());
+        for (AvroField field : record.getFields()) {
+            checkField(field);
+        }
+    }
+
+    private void checkField(AvroField field) {
+        String fieldName = field.getName();
+        switch (fieldName) {
+            case "aSimpleEnumMapList": {
+                assertEquals("java.util.List", field.getQualifiedName());
+                assertEquals(true, field.isNullable());
+                assertEquals(List.class, field.getAvroType().getType());
+                assertEquals(Schema.Type.ARRAY, field.getAvroType().getSchemaType());
+                assertEquals(null, field.getAvroType().getLogicalType());
+                assertEquals(Schema.Type.MAP, field.getAvroType().getItemType().getSchemaType());
+                assertEquals(Schema.Type.ENUM, field.getAvroType().getItemType().getValueType().getSchemaType());
+                assertEquals(null, field.getAvroType().getValueType());
+                break;
+            }
+            case "aBigDecimalSetListMap": {
+                assertEquals("java.util.Map", field.getQualifiedName());
+                assertEquals(true, field.isNullable());
+                assertEquals(Map.class, field.getAvroType().getType());
+                assertEquals(Schema.Type.MAP, field.getAvroType().getSchemaType());
+                assertEquals(null, field.getAvroType().getLogicalType());
+                assertEquals(null, field.getAvroType().getItemType());
+                assertEquals(Schema.Type.ARRAY, field.getAvroType().getValueType().getSchemaType());
+                assertEquals(Schema.Type.ARRAY, field.getAvroType().getValueType().getItemType().getSchemaType());
+                assertEquals(LogicalTypes.decimal(19, 2), field.getAvroType().getValueType().getItemType().getItemType().getLogicalType());
+                break;
+            }
+            case "aComplexClassListSetMapMapMap": {
+                assertEquals("java.util.Map", field.getQualifiedName());
+                assertEquals(false, field.isNullable());
+                assertEquals(Map.class, field.getAvroType().getType());
+                assertEquals(Schema.Type.MAP, field.getAvroType().getSchemaType());
+                assertEquals(null, field.getAvroType().getLogicalType());
+                assertEquals(null, field.getAvroType().getItemType());
+                assertEquals(Schema.Type.MAP, field.getAvroType().getValueType().getSchemaType());
+                assertEquals(Schema.Type.MAP, field.getAvroType().getValueType().getValueType().getSchemaType());
+                assertEquals(Schema.Type.ARRAY, field.getAvroType().getValueType().getValueType().getValueType().getSchemaType());
+                assertEquals(Schema.Type.ARRAY, field.getAvroType().getValueType().getValueType().getValueType().getItemType().getSchemaType());
+                assertEquals(Schema.Type.RECORD, field.getAvroType().getValueType().getValueType().getValueType().getItemType().getItemType().getSchemaType());
+                break;
+            }
+            case "aInheritedClassMapListMapMapSet": {
+                assertEquals("java.util.Set", field.getQualifiedName());
+                assertEquals(true, field.isNullable());
+                assertEquals(Set.class, field.getAvroType().getType());
+                assertEquals(Schema.Type.ARRAY, field.getAvroType().getSchemaType());
+                assertEquals(null, field.getAvroType().getLogicalType());
+                assertEquals(Schema.Type.MAP, field.getAvroType().getItemType().getSchemaType());
+                assertEquals(Schema.Type.MAP, field.getAvroType().getItemType().getValueType().getSchemaType());
+                assertEquals(Schema.Type.ARRAY, field.getAvroType().getItemType().getValueType().getValueType().getSchemaType());
+                assertEquals(Schema.Type.MAP, field.getAvroType().getItemType().getValueType().getValueType().getItemType().getSchemaType());
+                assertEquals(Schema.Type.RECORD, field.getAvroType().getItemType().getValueType().getValueType().getItemType().getValueType().getSchemaType());
+                assertEquals(null, field.getAvroType().getValueType());
+                break;
+            }
+            default: {
+                assertTrue("Should not come here!", false);
+            }
+        }
+    }
+}
--- src/test/java/com/nobodyhub/datalayer/core/cases/DeepContainer.java
+++ src/test/java/com/nobodyhub/datalayer/core/cases/DeepContainer.java
@@ -1,5 +1,6 @@
 package com.nobodyhub.datalayer.core.cases;
 
+import javax.persistence.Column;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
@@ -12,8 +13,12 @@ import java.util.Set;
  * @since 2018-04-20.
  */
 public class DeepContainer {
+    @Column
     private List<Map<String, SimpleEnum>> aSimpleEnumMapList;
+    @Column
     private Map<String, List<Set<BigDecimal>>> aBigDecimalSetListMap;
+    @Column(nullable = false)
     private Map<String, Map<String, Map<String, Set<List<ComplexClass>>>>> aComplexClassListSetMapMapMap;
+    @Column
     private Set<Map<String, Map<String, List<Map<String, InheritedClass>>>>> aInheritedClassMapListMapMapSet;
 }
