beb75a10200e45ab005bd73a065fc6ff79a00460
javis-yan <yan_h@hotmail.com>
2018-04-23 00:36:36 +0800

    Inherit ReflectData rather than writing own classes

---
+++ src/main/java/com/nobodyhub/datalayer/core/BigDecimalConversion.java
@@ -0,0 +1,28 @@
+package com.nobodyhub.datalayer.core;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.apache.avro.Conversions;
+import org.apache.avro.LogicalTypes;
+import org.apache.avro.Schema;
+
+/**
+ * @author Ryan
+ */
+@RequiredArgsConstructor
+@Getter
+public class BigDecimalConversion extends Conversions.DecimalConversion {
+
+    private final int precision;
+    private final int scale;
+
+    /**
+     * precison and scale are set according to {@link org.hibernate.mapping.Column}
+     *
+     * @return
+     */
+    @Override
+    public Schema getRecommendedSchema() {
+        return LogicalTypes.decimal(precision, scale).addToSchema(Schema.create(Schema.Type.BYTES));
+    }
+}
