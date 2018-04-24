51bcb859a1ef9f9f115f9f91f81719f55fa93287
yan_h <yan_h@worksap.co.jp>
2018-04-24 17:52:36 +0800

    add comments for RestrictionSetType

--- src/main/java/com/nobodyhub/datalayer/core/db/criteria/RestrictionSetType.java
+++ src/main/java/com/nobodyhub/datalayer/core/db/criteria/RestrictionSetType.java
@@ -5,6 +5,12 @@ package com.nobodyhub.datalayer.core.db.criteria;
  * @since 2018-04-23.
  */
 public enum RestrictionSetType {
+    /**
+     * Use <a href="https://en.wikipedia.org/wiki/Disjunctive_normal_form">DNF</a>v to organize the {@link RestrictionSet#restrictions}
+     */
     DISJUNCTION,
+    /**
+     * Use <a href="https://en.wikipedia.org/wiki/Conjunctive_normal_form">CNF</a> to organize the {@link RestrictionSet#restrictions}
+     */
     CONJUNCTION
 }
