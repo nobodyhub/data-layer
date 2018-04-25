dac68b77bd89d63f59b151c984ab4789d965303b
yan_h <yan_h@worksap.co.jp>
2018-04-25 16:26:01 +0800

    set QueryRequestData#cls as final

--- src/main/java/com/nobodyhub/datalayer/core/service/model/QueryRequestData.java
+++ src/main/java/com/nobodyhub/datalayer/core/service/model/QueryRequestData.java
@@ -2,13 +2,15 @@ package com.nobodyhub.datalayer.core.service.model;
 
 import com.nobodyhub.datalayer.core.db.criteria.Criteria;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 /**
  * @author yan_h
  * @since 2018-04-25.
  */
 @Data
+@RequiredArgsConstructor
 public class QueryRequestData<T> {
-    private Class<T> cls;
+    private final Class<T> cls;
     private Criteria criteria;
 }
