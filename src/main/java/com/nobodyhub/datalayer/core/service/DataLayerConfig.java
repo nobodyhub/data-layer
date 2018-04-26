c5b30695fc70c22001bef52965bda70d4e846cd8
javis-yan <yan_h@hotmail.com>
2018-04-26 09:43:07 +0800

    compose Hibernate configurations

--- src/main/java/com/nobodyhub/datalayer/core/service/DataLayerConfig.java
+++ src/main/java/com/nobodyhub/datalayer/core/service/DataLayerConfig.java
@@ -1,44 +1,52 @@
 package com.nobodyhub.datalayer.core.service;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.google.common.collect.Sets;
+import com.nobodyhub.datalayer.core.avro.AvroData;
+import com.nobodyhub.datalayer.core.service.common.AvroEntity;
 import com.nobodyhub.datalayer.core.service.common.AvroSchemaLoaderConfiguration;
-import org.hibernate.SessionFactory;
+import org.hibernate.c3p0.internal.C3P0ConnectionProvider;
+import org.hibernate.cfg.Configuration;
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 
-import javax.sql.DataSource;
-import java.beans.PropertyVetoException;
+import javax.persistence.Entity;
 import java.util.Properties;
+import java.util.Set;
 
 /**
  * @author Ryan
  */
-@AvroSchemaLoaderConfiguration(basePackages = "com.nobodyhub.datalayer.core")
 public class DataLayerConfig {
 
-    private String driverClassName;
-    private String url;
-    private String username;
-    private String password;
+    static {
+        Reflections configurationReflection = new Reflections(new ConfigurationBuilder()
+                .addUrls(ClasspathHelper.forClassLoader())
+                .setScanners(new TypeAnnotationsScanner(), new SubTypesScanner(false)));
+        Set<Class<?>> avroClass = Sets.newHashSet((configurationReflection.getSubTypesOf(AvroEntity.class)));
+        Set<Class<?>> entityClass = configurationReflection.getTypesAnnotatedWith(Entity.class);
 
-    private String hibernateDialect;
-    private String hibernateShowSql;
-    private String hibernateHbm2ddlAuto;
-
-    private String[] packageToScan;
-
-    private Properties hibernateProperties() {
+        Configuration hibernateConfig = new Configuration();
         Properties props = new Properties();
-        props.put("hibernate.dialect", hibernateDialect);
-        props.put("hibernate.show_sql", hibernateShowSql);
-        props.put("hibernate.hbm2ddl.auto", hibernateHbm2ddlAuto);
-        return props;
-    }
-
-    private DataSource dataSource() throws PropertyVetoException {
-        ComboPooledDataSource dataSource = new ComboPooledDataSource();
-        dataSource.setDriverClass(driverClassName);
-        dataSource.setJdbcUrl(url);
-        dataSource.setUser(username);
-        dataSource.setPassword(password);
-        return dataSource;
+        props.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        props.put("hibernate.show_sql", true);
+        props.put("hibernate.hbm2ddl.auto", true);
+        props.put("hibernate.connection.provider_class", C3P0ConnectionProvider.class);
+        props.put("hibernate.connection.driver_class", "org.h2.Driver");
+        props.put("hibernate.connection.url", "jdbc:h2:mem:test;INIT=RUNSCRIPT FROM 'classpath:init.sql';DB_CLOSE_ON_EXIT=FALSE");
+        props.put("hibernate.connection.username", "sa");
+        props.put("hibernate.connection.password", "");
+        hibernateConfig.setProperties(props);
+        //TODO: add C3p0 settings
+        //add entity class
+        for (Class cls : entityClass) {
+            hibernateConfig.addClass(cls);
+        }
+        avroClass.addAll(entityClass);
+        for (Class cls : avroClass) {
+            AvroData.get().getSchema(cls);
+        }
     }
 }
---
+++ src/test/java/com/nobodyhub/datalayer/core/service/DataLayerConfigTest.java
@@ -0,0 +1,16 @@
+package com.nobodyhub.datalayer.core.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Ryan
+ */
+public class DataLayerConfigTest {
+    @Test
+    public void test() {
+        new DataLayerConfig();
+    }
+
+}
