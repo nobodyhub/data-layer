ebecb32473ebb275faf7a2a4e231fc9a1e5a1db5
javis-yan <yan_h@hotmail.com>
2018-04-27 08:53:08 +0800

    add annotations for criteria

--- src/main/java/com/nobodyhub/datalayer/core/service/repository/criteria/ProjectionType.java
+++ src/main/java/com/nobodyhub/datalayer/core/service/repository/criteria/ProjectionType.java
@@ -1,6 +1,7 @@
 package com.nobodyhub.datalayer.core.service.repository.criteria;
 
 import com.nobodyhub.datalayer.core.service.common.AvroEntity;
+import org.hibernate.criterion.Projections;
 
 /**
  * Projections that supported by Hibernate Projection
@@ -10,12 +11,36 @@ import com.nobodyhub.datalayer.core.service.common.AvroEntity;
  * @since 2018-04-24.
  */
 public enum ProjectionType implements AvroEntity {
+    /**
+     * @see Projections#groupProperty(String)
+     */
     GROUP_PROPERTY,
+    /**
+     * @see Projections#rowCount()
+     */
     ROW_COUNT,
+    /**
+     * @see Projections#count(String)
+     */
     COUNT,
+    /**
+     * @see Projections#countDistinct(String)
+     */
     COUNT_DISTINCT,
+    /**
+     * @see Projections#max(String)
+     */
     MAX,
+    /**
+     * @see Projections#min(String)
+     */
     MIN,
+    /**
+     * @see Projections#avg(String)
+     */
     AVG,
+    /**
+     * @see Projections#sum(String)
+     */
     SUM
 }
