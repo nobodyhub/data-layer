ff64dbb48f83a1f967f579d3c37eaf1172fa37bb
javis-yan <yan_h@hotmail.com>
2018-04-28 23:50:02 +0800

    Use DynamicMessage to parse arbitary type

--- src/main/java/com/nobodyhub/datalayer/core/proto/ProtoData.java
+++ src/main/java/com/nobodyhub/datalayer/core/proto/ProtoData.java
@@ -4,14 +4,13 @@ import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.collect.Lists;
-import com.google.protobuf.DescriptorProtos;
-import com.google.protobuf.Descriptors;
-import com.google.protobuf.DynamicMessage;
-import com.google.protobuf.Message;
+import com.google.common.collect.Maps;
+import com.google.protobuf.*;
 
 import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -21,20 +20,30 @@ import java.util.concurrent.ExecutionException;
  */
 public class ProtoData {
 
-    private final LoadingCache<Class<?>, DescriptorProtos.DescriptorProto> protoCache =
+    protected final LoadingCache<Class<?>, Descriptors.Descriptor> protoCache =
             CacheBuilder.newBuilder()
                     .weakKeys()
-                    .build(new CacheLoader<Class<?>, DescriptorProtos.DescriptorProto>() {
+                    .build(new CacheLoader<Class<?>, Descriptors.Descriptor>() {
                         @Override
-                        public DescriptorProtos.DescriptorProto load(Class<?> key) throws Exception {
-                            return createProto(key);
+                        public Descriptors.Descriptor load(Class<?> key) throws Exception {
+                            return getMessegeDescriptor(key);
                         }
                     });
 
-    private DescriptorProtos.DescriptorProto createProto(Class<?> cls) {
+    protected final LoadingCache<Class<?>, List<Field>> fieldCache =
+            CacheBuilder.newBuilder()
+            .weakKeys()
+            .build(new CacheLoader<Class<?>, List<Field>>() {
+                @Override
+                public List<Field> load(Class<?> key) throws Exception {
+                    return getFields(key);
+                }
+            });
+
+    protected DescriptorProtos.DescriptorProto createProto(Class<?> cls) throws ExecutionException {
         DescriptorProtos.DescriptorProto.Builder protoBuilder = DescriptorProtos.DescriptorProto.newBuilder();
-        protoBuilder.setName(cls.getName());
-        List<Field> fields = getFields(cls);
+        protoBuilder.setName(cls.getSimpleName());
+        List<Field> fields = fieldCache.get(cls);
         int idx = 1;
         for (Field field : fields) {
             DescriptorProtos.FieldDescriptorProto protoField =
@@ -46,13 +55,14 @@ public class ProtoData {
         return protoBuilder.build();
     }
 
-    private List<Field> getFields(Class<?> cls) {
+    protected List<Field> getFields(Class<?> cls) {
         List<Field> fields = Lists.newArrayList();
         if (cls.isEnum()) {
 
         } else {
             for (Field field : cls.getDeclaredFields()) {
                 //TODO: add inherited field
+                field.setAccessible(true);
                 fields.add(field);
             }
             //because The elements returned by cls.getDeclaredFields()
@@ -64,20 +74,41 @@ public class ProtoData {
         return fields;
     }
 
-    private <T> Message convert(T instance) throws ExecutionException, Descriptors.DescriptorValidationException, IllegalAccessException {
-        DescriptorProtos.DescriptorProto proto = protoCache.get(instance.getClass());
-        DescriptorProtos.FileDescriptorProto fileDescP = DescriptorProtos.FileDescriptorProto.newBuilder()
-                .addMessageType(proto).build();
-
-        Descriptors.FileDescriptor dynamicDescriptor = Descriptors.FileDescriptor.buildFrom(fileDescP, new Descriptors.FileDescriptor[0]);
-
-        Descriptors.Descriptor msgDescriptor = dynamicDescriptor.findMessageTypeByName(instance.getClass().getName());
-        DynamicMessage.Builder dmBuilder =
-                DynamicMessage.newBuilder(msgDescriptor);
-        for (Field field : getFields(instance.getClass())) {
+    protected <T> DynamicMessage encode(T instance) throws ExecutionException, Descriptors.DescriptorValidationException, IllegalAccessException {
+        Descriptors.Descriptor msgDescriptor = protoCache.get(instance.getClass());
+        DynamicMessage.Builder dmBuilder = DynamicMessage.newBuilder(msgDescriptor);
+        for (Field field : fieldCache.get(instance.getClass())) {
             dmBuilder.setField(msgDescriptor.findFieldByName(field.getName()), field.get(instance));
         }
         return dmBuilder.build();
+    }
+
+    protected Descriptors.Descriptor getMessegeDescriptor(Class<?> cls) throws ExecutionException, Descriptors.DescriptorValidationException {
+        DescriptorProtos.DescriptorProto proto = createProto(cls);
+        DescriptorProtos.FileDescriptorProto fileDescP = DescriptorProtos.FileDescriptorProto.newBuilder()
+                .addMessageType(proto)
+                .setPackage(cls.getPackage().getName())
+                .build();
+
+        Descriptors.FileDescriptor dynamicDescriptor = Descriptors.FileDescriptor.buildFrom(fileDescP, new Descriptors.FileDescriptor[0]);
+
+        return dynamicDescriptor.findMessageTypeByName(cls.getSimpleName());
+    }
+
+    protected <T> void decode(T defaultInstance, ByteString data) throws ExecutionException, Descriptors.DescriptorValidationException, InvalidProtocolBufferException, IllegalAccessException, InstantiationException {
+        Descriptors.Descriptor msgDescriptor = protoCache.get(defaultInstance.getClass());
+        DynamicMessage.Builder dmBuilder = DynamicMessage.newBuilder(msgDescriptor);
+        DynamicMessage msg = dmBuilder.build().getParserForType().parseFrom(data);
+        Map<String, Field> fields = Maps.newHashMap();
+        for (Field field : fieldCache.get(defaultInstance.getClass())) {
+            fields.put(field.getName(), field);
+        }
+
+        for (Map.Entry<Descriptors.FieldDescriptor, Object> entry : msg.getAllFields().entrySet()) {
+            Descriptors.FieldDescriptor fieldDesp = entry.getKey();
+            Field field = fields.get(fieldDesp.getName());
+            field.set(defaultInstance, entry.getValue());
+        }
     }
 
     /**
@@ -85,12 +116,14 @@ public class ProtoData {
      * @return
      * @see <a href="https://developers.google.com/protocol-buffers/docs/proto3">Scalar Value Types</a>
      */
-    private DescriptorProtos.FieldDescriptorProto.Type getType(Class<?> type) {
+    protected DescriptorProtos.FieldDescriptorProto.Type getType(Class<?> type) {
         //TODO: add for all types
-        if (type == Double.class || type == Double.TYPE) {
-            return DescriptorProtos.FieldDescriptorProto.Type.TYPE_DOUBLE;
+        if (type == String.class) {
+            return DescriptorProtos.FieldDescriptorProto.Type.TYPE_STRING;
+        } else if (type == Integer.class || type == Integer.TYPE) {
+            return DescriptorProtos.FieldDescriptorProto.Type.TYPE_INT32;
         } else {
-            return DescriptorProtos.FieldDescriptorProto.Type.TYPE_BYTES;
+            return DescriptorProtos.FieldDescriptorProto.Type.TYPE_STRING;
         }
     }
 }
